273258cadac675b67609baf486f86ba70b9b1ffb
Diogo <diogosilvaa22@gmail.com>
2024-03-31 17:15:53 +0100

    Teste Automação
    User Page
    Reciclar

--- GlobalImpactAutomationTest/AutenticationTest.cs
+++ GlobalImpactAutomationTest/AutenticationTest.cs
@@ -1,5 +1,7 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace GlobalImpactAutomationTest
 {
@@ -77,5 +79,65 @@ namespace GlobalImpactAutomationTest
 
             driver.Quit();
         }
+
+        [Fact]
+        public void UserPage_Automation_Test()
+        {
+            driver.Url = "https://localhost:7154";
+            driver.FindElement(By.LinkText("LOGIN")).Click();
+
+            var username = driver.FindElement(By.Id("username"));
+            var password = driver.FindElement(By.Id("password"));
+
+            username.SendKeys("testuser");
+            
+            password.SendKeys("!Qq1234");
+
+            driver.FindElement(By.Id("login")).Click();
+
+            Thread.Sleep(2000);
+
+            driver.FindElement(By.LinkText("Profile")).Click();
+
+            Thread.Sleep(2000);
+            //driver.FindElement(By.Id("logout")).Click();
+
+            driver.Quit();
+        }
+
+        [Fact]
+        public void recycle_Automation_Test()
+        {
+            driver.Url = "https://localhost:7154";
+            driver.FindElement(By.LinkText("ECOPONTO")).Click();
+
+            Thread.Sleep(2000);
+
+            // Encontrar e clicar no primeiro botão "Submeter"
+            var submitButton = driver.FindElement(By.CssSelector(".btn-grad"));
+            submitButton.Click();
+
+            Thread.Sleep(2000);
+
+            var uniqueCode = driver.FindElement(By.Id("uniqueCode"));
+            uniqueCode.SendKeys("96ded3fd-e486-4ac7-a398-c2fae7942514");
+
+            Thread.Sleep(2000);
+
+			var submitButton2 = driver.FindElement(By.CssSelector(".btn-grad"));
+			submitButton2.Click();
+
+			Thread.Sleep(2000);
+
+			var submitButton3 = driver.FindElement(By.CssSelector(".btn-grad-small"));
+			submitButton3.Click();
+
+			//var submitButton4 = driver.FindElement(By.Id("terminar"));
+			//submitButton4.Click();
+
+			Thread.Sleep(2000);
+			//driver.Quit();
+		}
+
     }
 }
